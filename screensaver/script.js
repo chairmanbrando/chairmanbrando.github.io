b87d786b4632afd8097172230b5c5a21b224c64a
chairmanbrando <chairmanbrando@users.noreply.github.com>
2023-04-10 21:25:52 -0400

    Screensaver: Added support for many things at once
    I'm not sure the full-screen check will work on Macs; I wanna say they tend to leave the UI chrome in place.

--- screensaver/script.js
+++ screensaver/script.js
@@ -1,47 +1,72 @@
 const onDomEvent = document.addEventListener;
 const onWinEvent = window.addEventListener;
 
-const onDomReady = function (fn) {
+function onDomReady(fn) {
     (document.readyState === 'loading') ? onDomEvent('DOMContentLoaded', fn) : fn();
 }
 
-// Check and see and change the body class if so.
-const checkForFullscreen = function () {
-    if (window.innerHeight === screen.height) return true;
+function checkForFullscreen() {
+    return (window.innerWidth === screen.width && window.innerHeight === screen.height);
 };
+
+function getQueryVars() {
+    var split = window.location.search.substring(1).split('&'),
+        vars  = {};
+
+    split.forEach(function (i) {
+        var parts = i.split('=');
+
+        vars[parts[0]] = parts[1];
+    });
+
+    return vars;
+}
 
 function randInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-let canvas = null;
-let ctx    = null;
-let img    = new Image;
-let bGo    = false;
-let af     = null;
-
-let saver = {
-    x:  0,
-    y:  0,
-    dx: 2,
-    dy: 2,
-    img: new Image,
-    init: function () {
-        this.img.src    = "flop.png";
-        this.img.width  = 120;
-        this.img.height = 120;
-        this.x          = randInt(canvas.width - this.img.width);
-        this.y          = randInt(canvas.height - this.img.height);
-    },
-    draw: function () {
-        ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.height);
-    },
-    clear: function () {
-        ctx.clearRect(this.x, this.y, this.img.width, this.img.height);
-    }
-};
+function saverFactory() {
+    return {
+        x:  0,
+        y:  0,
+        dx: randInt(4) + 2,
+        dy: randInt(4) + 2,
+        img: new Image,
+        init: function () {
+            this.img.src    = "flop.png";
+            this.img.width  = 120;
+            this.img.height = 120;
+            this.x          = randInt(canvas.width - this.img.width);
+            this.y          = randInt(canvas.height - this.img.height);
+        },
+        draw: function () {
+            ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.height);
+        },
+        clear: function () {
+            ctx.clearRect(this.x, this.y, this.img.width, this.img.height);
+        },
+        checkBounds: function () {
+            if (this.x < 0 || this.x > (canvas.width - this.img.width)) {
+                this.dx *= -1;
+            }
+            
+            if (this.y < 0 || this.y > (canvas.height - this.img.height)) {
+                this.dy *= -1;
+            }
+        },
+        update: function () {
+            this.x += this.dx;
+            this.y += this.dy;
+        }
+    };
+}
 
 let savers = [];
+let canvas = null;
+let ctx    = null;
+let bGo    = false;
+let raf    = null;
 
 function init() {
     canvas = document.getElementById('canvas')
@@ -50,47 +75,43 @@ function init() {
     canvas.width = page.offsetWidth;
     canvas.height = page.offsetHeight;
 
-    saver.init();
-    saver.draw();
+    let num = parseInt(getQueryVars().n);
 
-    af = window.requestAnimationFrame(draw);
+    for (let i = 0; i < num; i++) {
+        savers.push(saverFactory());
+    }
+
+    savers.forEach(function (saver) {
+        saver.init();
+        saver.draw();
+    });
+
+    raf = window.requestAnimationFrame(draw);
 }
 
 function draw() {
-    saver.clear();
+    savers.forEach(function (saver) {
+        saver.clear();
+        saver.checkBounds();
+        saver.update();
+        saver.draw();
+    });
 
-    if (saver.x < 0) {
-        saver.dx *= -1;
-    }
-
-    if (saver.y < 0) {
-        saver.dy *= -1;
-    }
-
-    if (saver.x > (canvas.width - saver.img.width)) {
-        saver.dx *= -1;
-    }
-
-    if (saver.y > (canvas.height - saver.img.height)) {
-        saver.dy *= -1;
-    }
-
-    saver.x += saver.dx;
-    saver.y += saver.dy;
-
-    saver.draw();
-
-    af = window.requestAnimationFrame(draw);
+    raf = window.requestAnimationFrame(draw);
 }
 
 onWinEvent('resize', function (e) {
     canvas.width  = page.offsetWidth;
     canvas.height = page.offsetHeight;
+
+    (checkForFullscreen())
+        ? document.body.classList.remove('windowed')
+        : document.body.classList.add('windowed');
 });
 
 onDomEvent('keyup', function (e) {
     if (e.key === 'Escape') {
-        (bGo) ? af = window.requestAnimationFrame(draw) : window.cancelAnimationFrame(af);
+        (bGo) ? raf = window.requestAnimationFrame(draw) : window.cancelAnimationFrame(raf);
         bGo = ! bGo;
     }
 });
