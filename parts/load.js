c81ef2f607587fc91ae23e0448fac132501e6f09
chairmanbrando <chairmanbrando@users.noreply.github.com>
2024-11-22 16:58:31 -0500

    Added a JS partial-loading test
    Other things were adjusted due to the changes, but none have migrated over to include partials that way yet.

---
+++ parts/load.js
@@ -0,0 +1,21 @@
+/**
+ * Find and load all the template partials on the current page. When they're all
+ * loaded, add a class to the body to let everyone know.
+ */
+document.addEventListener('DOMContentLoaded', e => {
+    const proms = [];
+
+    document.querySelectorAll('div.partial').forEach(part => {
+        const url = part.getAttribute('data-partial');
+
+        proms.push(
+            fetch(url)
+                .then(response => response.text())
+                .then(html => {
+                    part.innerHTML = html;
+                })
+        );
+    });
+
+    Promise.all(proms).then(nada => document.body.classList.add('partials-loaded'));
+});
