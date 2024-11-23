141fac5ba4bb6454e606e0c011f8f63625ee83c4
Brandon <brandon@stovepipeco.com>
2024-11-22 23:57:42 -0500

    Screensaver: Size is now random

--- screensaver/script.js
+++ screensaver/script.js
@@ -5,6 +5,7 @@ function onDomReady(fn) {
     (document.readyState === 'loading') ? onDomEvent('DOMContentLoaded', fn) : fn();
 }
 
+// Yes, `URLSearchParams` exists but I forgot.
 function getQueryVars() {
     var split = window.location.search.substring(1).split('&'),
         vars  = {};
@@ -18,8 +19,8 @@ function getQueryVars() {
     return vars;
 }
 
-function randInt(max) {
-    return Math.floor(Math.random() * max);
+function randInt(min, max) {
+    return Math.floor(Math.random() * max) + min;
 }
 
 function isFullscreen() {
@@ -44,40 +45,47 @@ function saverFactory() {
     return {
         x: 0,
         y: 0,
-        dx: randInt(4) + 2,
-        dy: randInt(4) + 2,
+        dx: randInt(2, 6),
+        dy: randInt(2, 6),
         img: new Image,
         trail: false,
+
         init: function () {
             this.img.src    = "flop.png";
-            this.img.width  = 120;
-            this.img.height = 120;
-            this.x          = randInt(canvas.width - this.img.width);
-            this.y          = randInt(canvas.height - this.img.height);
+            this.img.width  = randInt(80, 160);
+            this.img.height = this.img.width;
+            this.x          = randInt(0, canvas.width - this.img.width);
+            this.y          = randInt(0, canvas.height - this.img.height);
         },
+
         draw: function () {
             ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.height);
         },
+
         clear: function () {
             if (! this.trail) {
                 ctx.clearRect(this.x, this.y, this.img.width, this.img.height);
             }
         },
+
         checkBounds: function () {
             if (this.x < 0 || this.x > (canvas.width - this.img.width)) {
                 this.dx *= -1;
             }
-            
+
             if (this.y < 0 || this.y > (canvas.height - this.img.height)) {
                 this.dy *= -1;
             }
         },
+
         update: function () {
             this.x += this.dx;
             this.y += this.dy;
         }
     };
 }
+
+// ---------------------------------------------------------------------------------------------- //
 
 let savers = [];
 let canvas = null;
@@ -87,9 +95,9 @@ let raf    = null;
 
 function init() {
     canvas = document.getElementById('canvas')
-    ctx = canvas.getContext('2d');
+    ctx    = canvas.getContext('2d');
 
-    canvas.width = page.offsetWidth;
+    canvas.width  = page.offsetWidth;
     canvas.height = page.offsetHeight;
 
     let num = parseInt(getQueryVars().n) || 1;
