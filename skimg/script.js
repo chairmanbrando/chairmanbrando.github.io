e994fd422eddb4a7f6752c629c601d2ad7eab93a
chairmanbrando <chairmanbrando@users.noreply.github.com>
2026-06-06 23:58:01 -0400

    Skimg: Added controls, changed defaults
    Also got rid of the Firefox warning because that 15-year-old bug was fixed!

--- skimg/script.js
+++ skimg/script.js
@@ -1,15 +1,18 @@
 const thing = document.querySelector('#thing span');
+const form  = document.querySelector('#form')
 
-// Yes, these are all variables in `window` automatically, but this is more clear.
-const text = document.querySelector('#text');
-const font = document.querySelector('#font');
-const size = document.querySelector('#size');
-const spac = document.querySelector('#spac');
-const line = document.querySelector('#line');
-const back = document.querySelector('#back');
+const text = form.elements.text;
+const font = form.elements.font;
+const size = form.elements.size;
+const colo = form.elements.colo;
+const spac = form.elements.spac;
+const line = form.elements.line;
+const shad = form.elements.shad;
+const back = form.elements.back;
 
-// Chrome isn't leaving the form values in place on refresh, so this bit ends up
-// being superfluous when using it. Meanwhile, gradients look bad in Firefox. 🤷‍♀️
+// Chromium isn't leaving the form values in place on refresh, perhaps because
+// the form element is missing some attribute it needs to save the data, so this
+// bit ends up being superfluous when using it. Ohwellio.
 thing.innerHTML                      = text.value;
 thing.style.fontFamily               = font.value;
 thing.style.fontSize                 = `${size.value}px`;
@@ -29,6 +32,10 @@ size.addEventListener('input', (e) => {
     thing.style.fontSize = `${e.target.value}px`;
 });
 
+colo.addEventListener('input', (e) => {
+    thing.style.color = e.target.value;
+});
+
 spac.addEventListener('input', (e) => {
     thing.style.letterSpacing = `${e.target.value}em`;
 });
@@ -37,6 +44,16 @@ line.addEventListener('input', (e) => {
     thing.style.lineHeight = e.target.value;
 });
 
+shad.addEventListener('input', (e) => {
+    thing.style.textShadow = e.target.value;
+});
+
 back.addEventListener('input', (e) => {
     thing.parentElement.style.background = e.target.value;
+});
+
+form.addEventListener('change', (e) => {
+    if (e.target.name === 'talign') {
+        thing.style.textAlign = e.target.value;
+    }
 });
